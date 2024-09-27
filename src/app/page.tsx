743c872cffd548eee7f1fca7fa7d24a6a8d5b695
nakari-noi <tichyajo@gmail.com>
2024-09-27 07:56:42 +0200

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <Typography variant="h1" gutterBottom>
+    h1. Heading
+    </Typography>
+
+  );
+}
