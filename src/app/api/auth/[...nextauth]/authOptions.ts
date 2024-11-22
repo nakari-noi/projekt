085ecabc27d9a360597645a855bcb633b4b064b1
nakari-noi <tichyajo@gmail.com>
2024-11-22 09:41:27 +0100

    zmena na vercel

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,12 +1,9 @@
-//src\app\api\auth\[...nextauth]\authOptions.ts
-
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-
 export const authOptions: NextAuthOptions = {
-  
-  providers: [
+
+    providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
